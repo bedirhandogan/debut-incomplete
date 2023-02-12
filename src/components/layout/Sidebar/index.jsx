e3ac6eece71df546f1d93d230c5d3e24a07f0b22
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-12 18:05:07 +0300

    refactor: some changes

--- src/components/layout/Sidebar/index.jsx
+++ src/components/layout/Sidebar/index.jsx
@@ -4,31 +4,31 @@ import Image from "assets";
 
 function Sidebar() {
     return (
-        <div className="sidebar">
-            <div className="sidebar-header">
+        <div className={"sidebar"}>
+            <div className={"sidebar-header"}>
                 <Image fileName={"logo.svg"} alt={"logo"} />
                 Debut
             </div>
             <div className="sidebar-body">
-                <Link to="/app">
+                <Link to={"/app"}>
                     <Image fileName={"home.svg"} alt={"introduction"} />
                     Introduction
                 </Link>
-                <Link to="tasks">
+                <Link to={"tasks"}>
                     <Image fileName={"tasks.svg"} alt={"tasks"} />
                     Task
                 </Link>
-                <Link to="notes">
+                <Link to={"notes"}>
                     <Image fileName={"note.svg"} alt={"notes"} />
                     Note
                 </Link>
-                <Link to="bookmarks">
+                <Link to={"bookmarks"}>
                     <Image fileName={"bookmark.svg"} alt={"bookmark"} />
                     Bookmark
                 </Link>
             </div>
 
-            <div className="sidebar-footer">
+            <div className={"sidebar-footer"}>
                 <Image fileName={"logo.svg"} alt={"picture"} />
                 {"username"}
                 <Image fileName={"logout.svg"} alt={"bookmark"} />

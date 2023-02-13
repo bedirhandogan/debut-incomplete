f52953cdf57934402f62b6e216fc001244cacc45
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-13 19:49:41 +0300

    refactor: convert styles to css module

--- src/components/layout/Sidebar/index.jsx
+++ src/components/layout/Sidebar/index.jsx
@@ -1,15 +1,15 @@
-import './styles.css';
+import styles from './styles.module.css';
 import {Link} from "react-router-dom";
 import Image from "assets";
 
 function Sidebar() {
     return (
-        <div className={"sidebar"}>
-            <div className={"sidebar-header"}>
+        <div className={styles.sidebar}>
+            <div className={styles.header}>
                 <Image fileName={"logo.svg"} alt={"logo"} />
                 Debut
             </div>
-            <div className="sidebar-body">
+            <div className={styles.body}>
                 <Link to={"/app"}>
                     <Image fileName={"home.svg"} alt={"introduction"} />
                     Introduction
@@ -28,7 +28,7 @@ function Sidebar() {
                 </Link>
             </div>
 
-            <div className={"sidebar-footer"}>
+            <div className={styles.footer}>
                 <Image fileName={"logo.svg"} alt={"picture"} />
                 {"username"}
                 <Image fileName={"logout.svg"} alt={"bookmark"} />

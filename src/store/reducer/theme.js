160599095069b034d91457a46c3844f177a54ac8
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-15 11:55:01 +0300

    feat: keep selected theme in localstorage

--- src/store/reducer/theme.js
+++ src/store/reducer/theme.js
@@ -3,11 +3,24 @@ import { createSlice } from '@reduxjs/toolkit'
 const theme = createSlice({
    name: 'themeSelector',
    initialState: {
-      theme: 'light',
+      theme: !localStorage.getItem('theme')
+         ? (() => {
+              localStorage.setItem('theme', 'light')
+              return 'light'
+           })()
+         : (() => {
+              document.children[0].setAttribute(
+                 'data-theme',
+                 localStorage.getItem('theme')
+              )
+              return localStorage.getItem('theme')
+           })(),
    },
    reducers: {
       change: (state) => {
          state.theme = state.theme === 'light' ? 'dark' : 'light'
+         localStorage.setItem('theme', state.theme)
+
          document.children[0].setAttribute('data-theme', state.theme)
       },
    },

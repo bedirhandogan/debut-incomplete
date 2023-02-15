24e2891ab90838dc7c2385a7353f5ea81dd9a3ed
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-15 13:30:25 +0300

    refactor: create app layout again

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../views/public/Home'
 import App from '../views/private/App'
+import Introduction from '../components/layout/Introduction'
 
 function Router() {
    return (
@@ -8,7 +9,7 @@ function Router() {
          <Routes>
             <Route path={'/'} element={<Home />} />
             <Route path={'app'} element={<App />}>
-               <Route path={'introduction'} element={<div>introduction</div>} />
+               <Route path={'/app'} element={<Introduction />} />
                <Route path={'tasks'} element={<div>tasks</div>} />
                <Route path={'notes'} element={<div>notes</div>} />
                <Route path={'bookmarks'} element={<div>bookmarks</div>} />
--- src/views/private/App.jsx
+++ src/views/private/App.jsx
@@ -1,11 +1,13 @@
 import Sidebar from 'components/layout/Sidebar'
 import Navbar from 'components/layout/Navbar'
+import { Outlet } from 'react-router-dom'
 
 function App() {
    return (
       <div className={'app'}>
          <Sidebar />
          <Navbar />
+         <Outlet />
       </div>
    )
 }

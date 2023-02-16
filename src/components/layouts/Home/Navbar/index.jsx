d295a98ff48fefd44a375111afb045d3126437aa
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-16 15:42:42 +0300

    feat(home): add navbar area

--- src/components/layouts/Home/Navbar/index.jsx
+++ src/components/layouts/Home/Navbar/index.jsx
@@ -1,7 +1,19 @@
-// import styles from './styles.module.css'
+import styles from './styles.module.css'
+import logo from 'assets/images/logo.svg'
 
 function Navbar() {
-   return <div className={'navbar'}></div>
+   return (
+      <div className={styles.navbar}>
+         <div className={styles.body}>
+            <img src={logo} alt={'logo'} />
+            <div className={styles.navItem}>Untitled</div>
+            <div className={styles.navItem}>Untitled</div>
+            <div className={styles.navItem}>Untitled</div>
+            <div className={styles.navItem}>Untitled</div>
+            <div className={styles.openAppBtn}>Open App</div>
+         </div>
+      </div>
+   )
 }
 
 export default Navbar

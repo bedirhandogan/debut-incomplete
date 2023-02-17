042dbeda2537ad3f388f2fd47ba33efc20d5e3fb
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-17 20:16:06 +0300

    refactor: some changes

--- src/components/shared/Modal/index.jsx
+++ src/components/shared/Modal/index.jsx
@@ -3,7 +3,7 @@ import { useCallback, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { change } from 'store/reducer/modal'
 
-function Modal({ size = { width: 350, height: 350 } }) {
+function Modal({ children, style }) {
    const { show } = useSelector((state) => state.modal)
    const dispatch = useDispatch()
    const [ref, refWrapper] = [useRef(), useRef()]
@@ -30,7 +30,9 @@ function Modal({ size = { width: 350, height: 350 } }) {
          style={{ display: show ? 'flex' : 'none' }}
          ref={ref}
       >
-         <div className={styles.wrapper} style={size} ref={refWrapper}></div>
+         <div className={styles.wrapper} style={style} ref={refWrapper}>
+            {children}
+         </div>
       </div>
    )
 }

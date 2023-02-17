f2f157866d9880d6acb7cd7552d9207177a01058
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-17 13:27:28 +0300

    chore(store): change theme slice name

--- src/store/reducer/theme.js
+++ src/store/reducer/theme.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const theme = createSlice({
-   name: 'themeSelector',
+   name: 'theme',
    initialState: {
       theme: !localStorage.getItem('theme')
          ? (() => {

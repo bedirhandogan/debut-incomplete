e19e687d508db6fec2ce4ae3cac9bd09b5d2f811
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-20 13:58:25 +0300

    style: some changes

--- src/components/shared/Input/index.jsx
+++ src/components/shared/Input/index.jsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css'
 
 function Input({ children, type, placeholder, style }) {
    return (
-      <div className={styles.search}>
+      <div className={styles.input}>
          {children}
          <input type={type} placeholder={placeholder} style={style} />
       </div>

12c1395b35fc974d72deb7672e9c086bfc7aaf94
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-26 14:46:11 +0300

    feat(app): add page title

--- src/components/layouts/App/Navbar/index.jsx
+++ src/components/layouts/App/Navbar/index.jsx
@@ -2,10 +2,20 @@ import styles from './styles.module.css'
 import { IconSearch } from '@tabler/icons-react'
 import Input from 'components/shared/Input'
 import ThemeSelector from 'components/shared/ThemeSelector'
+import { useLocation } from 'react-router-dom'
 
 function Navbar() {
+   const location = useLocation()
+
    return (
       <div className={styles.navbar}>
+         <div className={styles.area}>
+            <div className={styles.pageTitle}>
+               {location.pathname === '/app'
+                  ? 'Introduction'
+                  : location.pathname.slice(5)}
+            </div>
+         </div>
          <div className={styles.area}>
             <Input
                type={'text'}

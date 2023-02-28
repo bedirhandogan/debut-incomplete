ae3e3ed0b224a0d18df2e3b7a170e370fb099b34
bedirhandogan <bedirhandgn14@gmail.com>
2023-02-28 13:03:30 +0300

    feat(shared): add textarea component

---
+++ src/components/shared/Textarea/index.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+function Textarea(props) {
+   return (
+      <textarea className={styles.textarea} {...props}>
+         asdasdads
+      </textarea>
+   )
+}
+
+export default Textarea

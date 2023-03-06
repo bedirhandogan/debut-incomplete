f66312e3a33bdfba8d43705f324147d3ead9bcdf
bedirhandogan <bedirhandgn14@gmail.com>
2023-03-06 17:55:25 +0300

    feat(app/plans): add header

--- src/components/layouts/App/Plans/index.jsx
+++ src/components/layouts/App/Plans/index.jsx
@@ -1,9 +1,22 @@
 import './styles.scss'
 import PlanCard from 'components/shared/PlanCard'
+import { IconFilter } from '@tabler/icons-react'
 
 function Plans() {
    return (
       <div className={'plans'}>
+         <div className={'plans-header'}>
+            <div className={'plans-header-text'}>There is 1 plans</div>
+            <div className={'plans-header-filter'}>
+               <IconFilter
+                  stroke={1.3}
+                  width={20}
+                  height={20}
+                  style={{ color: 'var(--icon-color-primary)' }}
+               />
+               Filter
+            </div>
+         </div>
          <PlanCard />
       </div>
    )

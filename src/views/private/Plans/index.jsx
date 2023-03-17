8072725bb4db50e811493d807b52bab9b3d3d145
bedirhandogan <bedirhandgn14@gmail.com>
2023-03-17 21:37:57 +0300

    refactor(app/plans): use button component for filter

--- src/views/private/Plans/index.jsx
+++ src/views/private/Plans/index.jsx
@@ -2,6 +2,7 @@ import './styles.scss'
 import PlanCard from 'components/shared/PlanCard'
 import { IconFilter } from '@tabler/icons-react'
 import { useSelector } from 'react-redux'
+import Button from 'components/shared/Button'
 
 function Plans() {
    const plans = useSelector((state) => state.plans.data)
@@ -12,7 +13,13 @@ function Plans() {
             <div className={'plans-header-text'}>
                There is {plans?.length} plans
             </div>
-            <div className={'plans-header-filter'}>
+            <Button
+               style={{
+                  width: 'max-content',
+                  padding: '0 15px 0 10px',
+               }}
+               type={'fourth'}
+            >
                <IconFilter
                   stroke={1.3}
                   width={20}
@@ -20,7 +27,7 @@ function Plans() {
                   style={{ color: 'var(--icon-color-primary)' }}
                />
                Filter
-            </div>
+            </Button>
          </div>
 
          <div className={'plans-grid'}>

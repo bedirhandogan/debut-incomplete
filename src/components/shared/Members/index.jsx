f3caf293b9c986ae28958b4b42c44bfe848a4984
bedirhandogan <bedirhandgn14@gmail.com>
2023-03-25 17:43:29 +0300

    fix(shared/members): use optional chaining for undefined error

--- src/components/shared/Members/index.jsx
+++ src/components/shared/Members/index.jsx
@@ -2,11 +2,11 @@ import './styles.scss';
 
 function Members({ data }) {
   return (
-    <div className={'members'} data-member-length={data.users.length}>
-      {data.users.slice(0, 3).map((v, i) => (
+    <div className={'members'} data-member-length={data.users?.length}>
+      {data.users?.slice(0, 3).map((v, i) => (
         <img src={v.photoUrl} alt={v.displayName} key={i} />
       ))}
-      {data.users.length > 3 && <div className={'members-plus'}>+{data.users.length - 3}</div>}
+      {data.users?.length > 3 && <div className={'members-plus'}>+{data.users?.length - 3}</div>}
     </div>
   );
 }
